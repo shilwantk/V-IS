e81989863e6ec5c00ddc0d36362457cf8ca44142
Kirti S <shilwantkirti@gmail.com>
2025-02-28 12:36:47 +0530

    Initial Commit

---
+++ V-IS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  V-IS
+//
+//  Created by Kirti on 2/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ V-IS/V_ISApp.swift
@@ -0,0 +1,17 @@
+//
+//  V_ISApp.swift
+//  V-IS
+//
+//  Created by Kirti on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct V_ISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
